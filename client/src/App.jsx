2736196460e18d306a5a703f44b9ee4ce205a1ab
rizwandar <rizwandar@gmail.com>
2026-07-13 13:41:24 -0400

    Remove temporary Sentry debug page now that client integration is verified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,6 @@ import TermsPage              from './pages/TermsPage'
 import AccessibilityPage      from './pages/AccessibilityPage'
 import SecurityPage           from './pages/SecurityPage'
 import NotFoundPage           from './pages/NotFoundPage'
-import DebugSentryPage        from './pages/DebugSentryPage'
 import LegalDocumentsPage     from './pages/sections/LegalDocumentsPage'
 import FinancialAffairsPage   from './pages/sections/FinancialAffairsPage'
 import DigitalLifePage        from './pages/sections/DigitalLifePage'
@@ -507,7 +506,6 @@ function AppContent() {
           <Route path="/terms"             element={<TermsPage />} />
           <Route path="/accessibility"     element={<AccessibilityPage />} />
           <Route path="/security"          element={<SecurityPage />} />
-          <Route path="/_debug-sentry"     element={<DebugSentryPage />} />
           <Route path="/login"             element={<LoginPage />} />
           <Route path="/register"          element={<RegisterPage />} />
           <Route path="/forgot-password"   element={<ForgotPasswordPage />} />
